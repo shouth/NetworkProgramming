810d378588168cb67641fa7e2a4cd5b9b0fa9d73
Minami Shota <shouth373@gmail.com>
2021-05-09 22:25:08 +0900

    とりあえず提出に足るものが完成

--- task1/task1.c
+++ task1/task1.c
@@ -1,3 +1,54 @@
+/*
+    実行例1
+
+    コマンド
+    ./task1 http://www.google.com/
+
+    実行結果
+    Content length
+    -> Unknown (`Content-Length` field not found in response header)
+    Server software running at `www.google.com`
+    -> gws
+
+
+    実行例2
+
+    コマンド
+    ./task1 http://www.google.com/404
+
+    実行結果
+    Server returned 404 Not Found; Requested content `http://www.google.com/404` does not exist.
+
+    Content length
+    -> 1564
+    Server software running at `www.google.com`
+    -> Unknown (`Server` field not found in response header)
+
+
+    実行例3
+
+    コマンド
+    ./task1 http://www.is.kit.ac.jp/
+
+    実行結果
+    Content length
+    -> 233
+    Server software running at `www.is.kit.ac.jp`
+    -> Apache
+
+
+    実行例4
+
+    コマンド
+    ./task1 http://www.is.kit.ac.jp 143.198.109.222 3128
+
+    実行結果
+    Content length
+    -> 233
+    Server software running at `www.is.kit.ac.jp`
+    -> Apache
+ */
+
 #include <netdb.h>
 #include <netinet/in.h>
 #include <stdio.h>
@@ -9,34 +60,54 @@
 
 #define BUFSIZE 1024
 
-int get_rfc3986_host(char *uri, char *result)
+ssize_t gethostnamefromurl(char *buf, char *result)
 {
-    char *begin = strstr(uri, "://");
+    char *begin = strstr(buf, "://");
     if (begin == NULL) return -1;
     begin += 3;
 
     char *end = strstr(begin, "/");
-    if (end == NULL) end = strstr(begin, "?");
     if (end == NULL) end = strstr(begin, "#");
+    if (end == NULL) end = strstr(begin, "?");
     if (end == NULL) end = begin + strlen(begin);
 
-    char *itr = end;
-    while (itr != begin && *itr != ':') itr--;
-    if (begin != itr) end = itr;
-
-    if (*begin == '[' && *(end - 1) == ']') begin++, end--;
-
-    size_t size = end - begin;
+    ssize_t size = end - begin;
     strncpy(result, begin, size);
     return size;
 }
 
+ssize_t get_value_from_response(char *response, char *key, char *result)
+{
+    char *cur = strstr(response, key);
+    if (cur == NULL) return -1;
+
+    cur += strlen(key);
+    cur += strspn(cur, " :");
+    ssize_t size = strcspn(cur, " \r\n");
+    strncpy(result, cur, size);
+    result[size] = 0;
+
+    return size;
+}
+
+int get_status(char *response)
+{
+    char *cur = response;
+    cur += strcspn(cur, " ");
+    cur += strspn(cur, " ");
+
+    char status[4];
+    strncpy(status, cur, 3);
+    status[3] = 0;
+    return atoi(status);
+}
+
 int main(int argc, char **argv)
 {
-    char *content_uri = argv[1];
+    char *content_url = argv[1];
     char content_host_name[256];
-    if (get_rfc3986_host(content_uri, content_host_name) == -1) {
-        fprintf(stderr, "get_rfc3986_host()");
+    if (gethostnamefromurl(content_url, content_host_name) == -1) {
+        fprintf(stderr, "Failed to get host of content. URL may be malformed.");
         exit(EXIT_FAILURE);
     }
 
@@ -61,7 +132,7 @@ int main(int argc, char **argv)
     int strsize;
 
     if ((server_host = gethostbyname(server_host_name)) == NULL) {
-        fprintf(stderr, "gethostbyname()");
+        fprintf(stderr, "Failed to resolve host of `%s`. exit.", server_host_name);
         exit(EXIT_FAILURE);
     }
 
@@ -71,36 +142,57 @@ int main(int argc, char **argv)
     memcpy(&server_adrs.sin_addr, server_host->h_addr_list[0], server_host->h_length);
 
     if ((tcpsock = socket(PF_INET, SOCK_STREAM, 0)) == -1) {
-        fprintf(stderr, "socket()");
+        fprintf(stderr, "Failed to create socket. exit.\n");
         exit(EXIT_FAILURE);
     }
 
-    if (connect(tcpsock, (struct sockaddr*)&server_adrs, sizeof(server_adrs)) < 0) {
-        fprintf(stderr, "connect");
+    if (connect(tcpsock, (struct sockaddr*) &server_adrs, sizeof(server_adrs)) < 0) {
+        /* サーバーに接続できなかった場合 */
+        fprintf(stderr, "Failed to establish connection with `%s`. exit.\n", server_host_name);
         exit(EXIT_FAILURE);
     }
 
     s_buf[0] = 0;
     strsize = 0;
-    strsize += snprintf(k_buf, BUFSIZE, "GET %s HTTP/1.1\r\n", content_uri);
+    strsize += snprintf(k_buf, BUFSIZE, "GET %s HTTP/1.1\r\n", content_url);
     strncat(s_buf, k_buf, BUFSIZE);
     strsize += snprintf(k_buf, BUFSIZE, "Host: %s\r\n\r\n", content_host_name);
     strncat(s_buf, k_buf, BUFSIZE);
 
-    printf("%s", s_buf);
-
     if (send(tcpsock, s_buf, strsize + 1, 0) == -1) {
-        fprintf(stderr, "send()");
+        fprintf(stderr, "Failed to send request.");
         exit(EXIT_FAILURE);
     }
 
     if ((strsize = recv(tcpsock, r_buf, BUFSIZE - 1, 0)) == -1) {
-        fprintf(stderr, "recv()");
+        fprintf(stderr, "Failed to receive response.");
         exit(EXIT_FAILURE);
     }
     r_buf[strsize] = '\0';
 
-    printf("%s", r_buf);
+    int status = get_status(r_buf);
+    if (status == 404) {
+        /* 指定したコンテンツが存在しなかった場合 -> サーバーが404 Not Foundを返したときと解釈 */
+        printf("Server returned 404 Not Found; Requested content `%s` does not exist.\n\n", content_url);
+    }
+
+    ssize_t size;
+
+    size = get_value_from_response(r_buf, "Content-Length", k_buf);
+    printf("Content length\n-> ");
+    if (size >= 0) {
+        printf("%s\n", k_buf);
+    } else {
+        printf("Unknown (`Content-Length` field not found in response header)\n");
+    }
+
+    size = get_value_from_response(r_buf, "Server", k_buf);
+    printf("Server software running at `%s`\n-> ", content_host_name);
+    if (size >= 0) {
+        printf("%s\n", k_buf);
+    } else {
+        printf("Unknown (`Server` field not found in response header)\n");
+    }
 
     close(tcpsock);
     exit(EXIT_SUCCESS);
