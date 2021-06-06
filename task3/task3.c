6586b1253323f39acc3863dd49a1fccb6b1edf9c
Minami Shota <shouth373@gmail.com>
2021-06-07 00:34:04 +0900

    リネーム

--- task3/task3.c
+++ task3/task3.c
@@ -1,105 +1,104 @@
 #include "mynet.h"
-#include <stdio.h>
+#include <sys/wait.h>
+#include <pthread.h>
 
-#define BUFSIZE 1024
+#define BUFSIZE 50 /* バッファサイズ */
 
-int main(int argc, char const *argv[])
+void echo(int sock_listen);
+void *echo_thread(void *arg);
+
+int main(int argc, char *argv[])
 {
-    in_port_t port;
-    if (argc == 1) {
-        port = 50000;
-    } else {
-        port = atoi(argv[1]);
+    int port_number;
+    int parallel_type;
+    int connection_limit;
+
+    int sock_listen;
+
+    pid_t child;
+
+    int *tharg;
+    pthread_t tid;
+
+    int i;
+
+    if (argc != 4) {
+        fprintf(stderr,
+            "Usage: %s port_number parallel_type connection_limit\n"
+            "    port_number : The port number to listen.\n"
+            "    parallel_type : If 0 is specified, fork() is used. If 1 is specified, thread is used.\n"
+            "    connection_limit : The maximum number of connection to process simultaneously.\n",
+            argv[0]);
+        exit(EXIT_FAILURE);
     }
 
-    int sock_listen = init_tcpserver(port, 10);
+    port_number = atoi(argv[1]);
+    parallel_type = atoi(argv[2]);
+    connection_limit = atoi(argv[3]);
 
-    while (1) {
-        int sock_accepted = accept(sock_listen, NULL, NULL);
+    sock_listen = init_tcpserver(port_number, 5);
 
-        char buf[BUFSIZE];
-        int strsize;
-        FILE *fp;
-
-        sprintf(buf, "password: ");
-        send(sock_accepted, buf, strlen(buf), 0);
-
-        if ((strsize = recv(sock_accepted, buf, BUFSIZE, 0)) == -1) {
-            exit_errmesg("recv()");
+    if (parallel_type == 0) {
+        for (i = 0; i < connection_limit; i++) {
+            if ((child = fork()) != 0) {
+                echo(sock_listen);
+                // echo()は処理を返さないので子プロセスがこれ以上fork()を行うことはない
+            } else if (child < 0) {
+                exit_errmesg("fork()");
+            }
         }
-
-        buf[strsize] = 0;
-
-        const char *password = "password";
-        if (strncmp(buf, password, strlen(password)) != 0) {
-            sprintf(buf, "Sorry, password is incorrect.\r\n");
-            send(sock_accepted, buf, strlen(buf), 0);
-            close(sock_accepted);
-            continue;
+    } else if (parallel_type == 1) {
+        for (i = 0; i < connection_limit; i++) {
+            if ((tharg = (int *) malloc(sizeof(int))) == NULL) {
+                exit_errmesg("malloc()");
+            }
+            *tharg = sock_listen;
+            // 今回はthread_tを複数の処理で取りまわす必要がないため，変数tidは
+            // pthread_createを呼び出すためのプレースホルダとしての使用にとどまっている
+            if (pthread_create(&tid, NULL, echo_thread, (void *) tharg) != 0) {
+                exit_errmesg("pthread_create()");
+            }
         }
+    }
 
-        sprintf(buf, "Welcome, authorized personel.\r\n");
-        send(sock_accepted, buf, strlen(buf), 0);
+    for (;;);
+}
 
-        while (1) {
-            send(sock_accepted, "> ", 2, 0);
+int thread_id = 0;
+
+void echo(int sock_listen)
+{
+    int sock_accepted;
+    char buf[BUFSIZE];
+    int strsize;
+
+    // スレッドで並行処理を行っているときに出力を見分けるための変数
+    int this_thread_id = thread_id++;
+
+    for (;;) {
+        sock_accepted = accept(sock_listen, NULL, NULL);
+        printf("client is accepted [pid = %d, thread_id = %d]\n", getpid(), this_thread_id);
+        do {
             if ((strsize = recv(sock_accepted, buf, BUFSIZE, 0)) == -1) {
                 exit_errmesg("recv()");
             }
 
-            buf[strsize] = 0;
-
-            if (strncmp(buf, "list", 4) == 0) {
-                fp = popen("ls ~/work", "r");
-                fgets(buf, BUFSIZE, fp);
-
-                while (!feof(fp)) {
-                    fgets(buf, BUFSIZE, fp);
-                    send(sock_accepted, buf, strlen(buf), 0);
-                }
-
-                if (fclose(fp) != 0) {
-                    sprintf(buf, "Directory not found.\r\n");
-                    send(sock_accepted, buf, strlen(buf), 0);
-                }
-            } else if (strncmp(buf, "type", 4) == 0) {
-                char *cur = buf + 4;
-                cur += strspn(cur, " ");
-                int size = strcspn(cur, " \r\n");
-                cur[size] = 0;
-
-                if (strncmp(cur, "..", 2) == 0 || strstr(cur, "../") != NULL) {
-                    sprintf(buf, "Sorry, using `..` as parent directory is not allowed.\r\n");
-                    send(sock_accepted, buf, strlen(buf), 0);
-                    continue;
-                }
-
-                char command[256];
-                sprintf(command, "cat ~/work/%s", cur);
-                fp = popen(command, "r");
-                fgets(buf, BUFSIZE, fp);
-
-                while (!feof(fp)) {
-                    fgets(buf, BUFSIZE, fp);
-                    send(sock_accepted, buf, strlen(buf), 0);
-                }
-
-                if (fclose(fp) != 0) {
-                    sprintf(buf, "No such file.\r\n");
-                    send(sock_accepted, buf, strlen(buf), 0);
-                }
-            } else if (strncmp(buf, "exit", 4) == 0) {
-                sprintf(buf, "Goodbye.\r\n");
-                send(sock_accepted, buf, strlen(buf), 0);
-                break;
-            } else {
-                sprintf(buf, "Command not found.\r\n");
-                send(sock_accepted, buf, strlen(buf), 0);
+            if (send(sock_accepted, buf, strsize, 0) == -1) {
+                exit_errmesg("send()");
             }
-        }
-
+        } while (buf[strsize - 1] != '\n');
         close(sock_accepted);
     }
+}
 
-    return 0;
+void *echo_thread(void *arg)
+{
+    int sock_listen = *((int *) arg);
+    free(arg);
+
+    pthread_detach(pthread_self());
+
+    echo(sock_listen);
+
+    return (NULL);
 }
